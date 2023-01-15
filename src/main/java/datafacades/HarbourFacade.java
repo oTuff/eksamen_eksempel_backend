af66f46a0245449377826fd1c9dd1594bf12b3c2
oTuff <oscartuff@protonmail.com>
2023-01-16 00:59:17 +0100

    trying again

--- src/main/java/datafacades/HarbourFacade.java
+++ src/main/java/datafacades/HarbourFacade.java
@@ -28,10 +28,9 @@ public class HarbourFacade {
         return emf.createEntityManager();
     }
 
-    public Harbour createOwner(Harbour harbour) throws API_Exception {
-        return null;
-
-    }
+//    public Harbour createOwner(Harbour harbour) throws API_Exception {
+//        return null;
+//    }
 
     public List<Harbour> getAllHarbours() throws API_Exception {
         EntityManager em = getEntityManager();
